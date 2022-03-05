01e3a5a46525339826761c0780d8a6d307906fcc
Urusai-Teme <85540399+Urusai-Teme@users.noreply.github.com>
2022-03-05 21:59:08 +0800

    Add files via upload

---
+++ components/Logo.vue
@@ -0,0 +1,6 @@
+<template>
+  <img src="https://www.nicepng.com/png/full/30-300142_dollar-sign-white-png.png" 
+  width="100"
+  height="169" 
+  alt="the logo"/>
+</template>
---
+++ components/Textbox.vue
@@ -0,0 +1,51 @@
+<template>
+<div id="textboxWrapper" class="col-lg-12 control-section">
+    <div class="textbox-label"> Two-way binding</div>
+    <div class="col-lg-6">
+        <div id="wrapper">
+            <ejs-textbox floatLabelType="Auto" placeholder="Enter a value" v-model="value"></ejs-textbox>
+        </div>
+    </div> 
+    <div class="col-lg-6">
+        <div id="wrapper">
+            <ejs-textbox floatLabelType="Auto" placeholder="Enter a value" v-model="value"></ejs-textbox>
+        </div>
+    </div>
+
+</div>
+</template>
+
+<style scoped>
+    #wrapper {
+    max-width: 246px;
+    margin: 30px auto;
+    padding-top: 15px;
+    }
+
+    #textboxWrapper .textbox-label {
+    text-align: center;
+    width: 200px;
+    margin: 0 auto;
+    font-size: 14px;
+    user-select: none;
+    font-weight: 500;
+    font-family: "Roboto", "Segoe UI", "GeezaPro", "DejaVu Serif", "sans-serif",
+        "-apple-system", "BlinkMacSystemFont";
+    }
+</style>
+
+
+<script>
+import Vue from "vue";
+import { TextBoxPlugin } from '@syncfusion/ej2-vue-inputs';
+Vue.use(TextBoxPlugin);
+
+export default Vue.extend({
+    data: function() {
+        return {
+            value: null
+         }
+    }
+});
+
+</script>
