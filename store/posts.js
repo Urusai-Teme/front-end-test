e89dc39aac88ccccdc78daa05b9c8080dadb20e8
Urusai-Teme <85540399+Urusai-Teme@users.noreply.github.com>
2022-03-05 22:04:46 +0800

    Add files via upload

---
+++ store/posts.js
@@ -0,0 +1,20 @@
+export const state = () => ({
+  all: [
+    {
+      id: 'Savings Account',
+      title: 'Savings Account',
+      content: 'Could add value here'
+    },
+    {
+      id: 'Goals Account',
+      title: 'Goals Account',
+      content: 'Could show value here'
+    },
+    {
+      id: 'Create Account',
+      title: 'Create Account',
+      content: 'Could place textbox'
+    }
+  ]
+})
+
